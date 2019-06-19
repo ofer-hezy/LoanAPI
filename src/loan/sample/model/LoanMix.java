822d6c9afb8efb9ae22e81d444da7e80c6fe0923
hezy.ofer@gmail.com <hezy.ofer@gmail.com>
2019-06-19 11:54:39 +0300

    align code

--- src/loan/sample/model/LoanMix.java
+++ src/loan/sample/model/LoanMix.java
@@ -4,60 +4,75 @@ public class LoanMix {
 
 	private int autoInterest = 1;
 	private int loanBoard = 1;
-	public int getLoanBoard() {
-		return loanBoard;
-	}
-	public void setLoanBoard(int loanBoard) {
-		this.loanBoard = loanBoard;
-	}
 	private String loanType = "1|0";
 	private int loanValue = 0;
 	private int loanYears = 0;
 	private float loanInterest = 0;
 	private int loanGracepart = 0;
- public LoanMix(int autoInterest,String loanType,int loanYears,int loanValue,float loanInterest,int loanGracepart) {
-	 this.autoInterest = autoInterest;
-	 this.loanType = loanType;
-	 this.loanValue = loanValue;
-	 this.loanYears = loanYears;
-	 this.loanInterest = loanInterest;
-	 this.loanGracepart = loanGracepart;	 
- }
-public int getAutoInterest() {
-	return autoInterest;
+
+	public LoanMix(int autoInterest, String loanType, int loanYears, int loanValue, float loanInterest,
+			int loanGracepart) {
+		this.autoInterest = autoInterest;
+		this.loanType = loanType;
+		this.loanValue = loanValue;
+		this.loanYears = loanYears;
+		this.loanInterest = loanInterest;
+		this.loanGracepart = loanGracepart;
+	}
+
+	public int getAutoInterest() {
+		return autoInterest;
+	}
+
+	public void setAutoInterest(int autoInterest) {
+		this.autoInterest = autoInterest;
+	}
+
+	public String getLoanType() {
+		return loanType;
+	}
+
+	public void setLoanType(String loanType) {
+		this.loanType = loanType;
+	}
+
+	public int getLoanValue() {
+		return loanValue;
+	}
+
+	public void setLoanValue(int loanValue) {
+		this.loanValue = loanValue;
+	}
+
+	public int getLoanYears() {
+		return loanYears;
+	}
+
+	public void setLoanYears(int loanYears) {
+		this.loanYears = loanYears;
+	}
+
+	public float getLoanInterest() {
+		return loanInterest;
+	}
+
+	public void setLoanInterest(float loanInterest) {
+		this.loanInterest = loanInterest;
+	}
+
+	public int getLoanGracepart() {
+		return loanGracepart;
+	}
+
+	public void setLoanGracepart(int loanGracepart) {
+		this.loanGracepart = loanGracepart;
+	}
+
+	public int getLoanBoard() {
+		return loanBoard;
+	}
+
+	public void setLoanBoard(int loanBoard) {
+		this.loanBoard = loanBoard;
+	}
 }
-public void setAutoInterest(int autoInterest) {
-	this.autoInterest = autoInterest;
-}
-public String getLoanType() {
-	return loanType;
-}
-public void setLoanType(String loanType) {
-	this.loanType = loanType;
-}
-public int getLoanValue() {
-	return loanValue;
-}
-public void setLoanValue(int loanValue) {
-	this.loanValue = loanValue;
-}
-public int getLoanYears() {
-	return loanYears;
-}
-public void setLoanYears(int loanYears) {
-	this.loanYears = loanYears;
-}
-public float getLoanInterest() {
-	return loanInterest;
-}
-public void setLoanInterest(float loanInterest) {
-	this.loanInterest = loanInterest;
-}
-public int getLoanGracepart() {
-	return loanGracepart;
-}
-public void setLoanGracepart(int loanGracepart) {
-	this.loanGracepart = loanGracepart;
-}
-}
- 
